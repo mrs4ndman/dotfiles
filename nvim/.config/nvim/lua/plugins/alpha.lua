179de5658a0ea6e0c69a54e1d13df8a15dd228e8
MrSandman <mrsandman.h4sh@gmail.com>
2023-05-29 01:02:13 +0200

    modified gitignore and nvim dashboard

--- nvim/.config/nvim/lua/plugins/alpha.lua
+++ nvim/.config/nvim/lua/plugins/alpha.lua
@@ -1,5 +1,9 @@
+local options
 local alpha = require("alpha")
 local dashboard = require("alpha.themes.dashboard")
+
+if (vim.api.nvim_exec('echo argc()', true) == "0")
+then
 
 -- Header section config
 dashboard.section.header.val = {
@@ -12,14 +16,25 @@ dashboard.section.header.val = {
 " ██║ ╚═╝ ██║██║  ██║███████║██║  ██║██║ ╚████║██████╔╝██║ ╚═╝ ██║██║  ██║██║ ╚████║ ",
 " ╚═╝     ╚═╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝╚═╝  ╚═══╝╚═════╝ ╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝ ",
 "                                                                                    ",
+"                                   N E O V I M                                      ",
+"                                                                                    ",
+"                                      ／|__                                         ",
+"                                     (o o /                                         ",
+"                                      |.   ~.                                       ",
+"                                      じしf_,)ノ                                    ",
+
 }
 
 dashboard.section.buttons.val = {
-    dashboard.button( "e", "  > New file" , ":ene <BAR> startinsert <CR>"),
-    dashboard.button( "f", "  > Find file", ":cd $HOME | Telescope find_files<CR>"),
-    dashboard.button( "r", "  > Recent"   , ":Telescope oldfiles<CR>"),
-    dashboard.button( "s", "  > Settings" , ":e $NVIMRC<CR>"),
-    dashboard.button( "q", "  > Quit NVIM", ":qa<CR>"),
+    dashboard.button( "n", "    New file" ,             ":tabnew<CR>"),
+    dashboard.button( "e", "󰝰    Ex",                    ":Ex<CR>"),
+    dashboard.button( "f", "    Find project file",     ":Telescope find_files<CR>"),
+    dashboard.button( "h", "󱂵    Home dir find",         ":cd $HOME | Telescope find_files<CR>"),
+    dashboard.button( "r", "    Recent"   ,             ":Telescope oldfiles<CR>"),
+    dashboard.button( "g", "    Grep word",             ":Telescope live_grep<CR>"),
+    dashboard.button( "l", "󰒲    Lazy",                  ":Lazy<CR>"),
+    dashboard.button( "s", "    Main settings",         ":e $NVIMRC<CR>"),
+    dashboard.button( "q", "󰿅    Quit",                  ":qa<CR>"),
 }
 
 alpha.setup(dashboard.opts)
@@ -27,3 +42,6 @@ alpha.setup(dashboard.opts)
 vim.cmd([[
     autocmd Filetype alpha setlocal nofoldenable signcolumn=no nonumber norelativenumber
 ]])
+
+end
+
