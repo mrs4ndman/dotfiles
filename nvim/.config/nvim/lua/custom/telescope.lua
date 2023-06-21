8be8da3902583650ccfa47acacb062e27e49516c
MrSandman <mrsandman.h4sh@gmail.com>
2023-06-21 20:49:01 +0200

    tweak: moved around telescope conf

--- nvim/.config/nvim/lua/custom/telescope.lua
+++ nvim/.config/nvim/lua/custom/telescope.lua
@@ -1,6 +1,80 @@
 
 -- TELESCOPE
-local builtin = require('telescope.builtin')
+require("telescope").setup {
+    pickers = {
+        colorscheme = {
+            enable_preview = true,
+        }
+    },
+    extensions = {
+        fzf = {
+            fuzzy = true,
+            override_generic_sorter = true,
+            override_file_sorter = true,
+            case_mode = "smart_case",
+        },
+    },
+    defaults = {
+        preview = {
+            timeout = 500,
+        },
+        vimgrep_arguments = {
+            "rg",
+            "--no-heading",
+            "--with-filename",
+            "--line-number",
+            "--column",
+            "--smart-case",
+            "--hidden",
+        },
+        sort_mru = true,
+        sorting_strategy = "ascending",
+        color_devicons = true,
+        layout_config = {
+            prompt_position = 'top',
+            horizontal = {
+                width_padding = 0.04,
+                height_padding = 0.1,
+                preview_width = 0.6,
+            },
+            vertical = {
+                width_padding = 0.05,
+                height_padding = 1,
+                preview_height = 0.5,
+            },
+        },
+        border = true,
+        prompt_prefix = "   ",
+        hl_result_eol = false,
+        results_title = "",
+        winblend = 20,
+        wrap_results = true,
+        file_ignore_patterns = {},
+        hidden = true,
+        mappings = {
+            i = {
+                ["<C-n>"] = require("telescope.actions").cycle_history_next,
+                ["<C-p>"] = require("telescope.actions").cycle_history_prev,
+                ["<C-c>"] = require("telescope.actions").close,
+                ["<C-q>"] = require("telescope.actions").send_to_qflist,
+                ["<C-i>"] = require("telescope.actions").select_horizontal,
+                ["<C-x>"] = require("telescope.actions").select_vertical,
+                ["<C-u>"] = false,
+                ["<C-d>"] = false,
+            },
+            n = {
+                ["<C-n>"] = require("telescope.actions").cycle_history_next,
+                ["<C-p>"] = require("telescope.actions").cycle_history_prev,
+                ["<C-c>"] = require("telescope.actions").close,
+                ["<C-q>"] = require("telescope.actions").send_to_qflist,
+                ["<C-i>"] = require("telescope.actions").select_horizontal,
+                ["<C-x>"] = require("telescope.actions").select_vertical,
+            },
+        },
+    }
+}
+
+local builtin = require("telescope.builtin")
 
 -- Per-plugin which-key customization
 local wk = require("which-key")
@@ -18,6 +92,5 @@ wk.register({
 
 -- Extension invoking:
 
-pcall(require('telescope').load_extension 'fzf')
-pcall(require('telescope').load_extension 'neoclip')
-
+pcall(require("telescope").load_extension "fzf")
+pcall(require("telescope").load_extension "neoclip")
