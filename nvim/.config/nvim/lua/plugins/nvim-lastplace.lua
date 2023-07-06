d1277105a3a129db83e46e5f12fe0fdf7bb6fa88
MrSandman <mrsandman.h4sh@gmail.com>
2023-07-06 03:06:23 +0200

    add: moved lastplace to lua + rainbow delimiters :)

--- nvim/.config/nvim/lua/plugins/nvim-lastplace.lua
+++ nvim/.config/nvim/lua/plugins/nvim-lastplace.lua
@@ -1,12 +1,16 @@
+
 Customize = require("mrsandman.customize")
 local functions = require("mrsandman.functions")
 Is_Enabled = functions.is_enabled
 Use_Defaults = functions.use_plugin_defaults
 
-local plugin = "vim-lastplace"
+local plugin = "nvim-lastplace"
 
 return {
-  "farmergreg/" .. plugin,
+  "ethanholz/" .. plugin,
   event = "VimEnter",
   enabled = Is_Enabled(plugin),
+  config = function()
+    require("nvim-lastplace").setup{}
+  end
 }
