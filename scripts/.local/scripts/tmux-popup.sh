9f61f135bdb2e69d71853c2fba95fb608cae7b1b
MrSandman <121260905+mrs4ndman@users.noreply.github.com>
2023-09-02 01:02:06 +0200

    add: optimising fzf and tmux

--- scripts/.local/scripts/tmux-popup.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-if [ "$(tmux display-message -p -F "#{session_name}")" = "popup" ]; then
-    tmux detach-client
-else
-    tmux display-popup -E "tmux attach -t popup || tmux new -s popup"
-fi
