a6e9389cc2e5e3244e4cdf4c878d6a8bed854cd7
Juan Mañanes <121260905+mrs4ndman@users.noreply.github.com>
2026-01-29 23:40:08 +0100

    add: some i3status config + btop and tmux-sessionizer updates

--- i3/.config/i3status/bar_add_on.sh
+++ i3/.config/i3status/bar_add_on.sh
@@ -3,8 +3,8 @@ i3status --config /home/mrsandman/.config/i3status/config | while :
 do
     read -r line
     brightness=$(brightnessctl i | grep -oP '\(\K[0-9]+(?=%\))')    
-    playing_title=$(playerctl --player=spotify metadata title)
-    playing_subtitle=$(playerctl --player=spotify metadata artist)
+    playing_title=$(playerctl --player=presto metadata title)
+    playing_subtitle=$(playerctl --player=presto metadata artist)
     if [[ -z "$playing_title" || -z "$playing_subtitle" ]]; then
         echo "Nothing playing | 💡$brightness% | $line" || exit 1
     else
