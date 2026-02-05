b4d3b433d7a59d884170c3ff9d65a4765fe24581
Juan Mañanes <121260905+mrs4ndman@users.noreply.github.com>
2026-02-05 01:13:16 +0100

    add: presto config + update alacritty config

--- i3/.config/i3status/bar_add_on.sh
+++ i3/.config/i3status/bar_add_on.sh
@@ -4,10 +4,10 @@ do
     read -r line
     brightness=$(brightnessctl i | grep -oP '\(\K[0-9]+(?=%\))')    
     playing_title=$(playerctl --player=presto metadata title)
-    playing_subtitle=$(playerctl --player=presto metadata artist)
-    if [[ -z "$playing_title" || -z "$playing_subtitle" ]]; then
+    playing_artist=$(playerctl --player=presto metadata artist)
+    if [[ -z "$playing_title" || -z "$playing_artist" ]]; then
         echo "Nothing playing | 💡$brightness% | $line" || exit 1
     else
-        echo ">_< | 🎹 $playing_title < $playing_subtitle | 💡$brightness% | $line" || exit 1
+        echo ">_< | 🎹 $playing_title < $playing_artist | 💡$brightness% | $line" || exit 1
     fi
 done
